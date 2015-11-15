d581efef89876c67b2170dde08448ac7236b96fd
birelandef <r-sophiya@yandex.ru>
2015-11-15 18:40:46 +0300

    [Add] Page without json, webjars

---
+++ build.sbt
@@ -0,0 +1,21 @@
+name := "UIredactor"
+
+version := "1.0"
+
+lazy val `uiredactor` = (project in file(".")).enablePlugins(PlayScala)
+
+scalaVersion := "2.11.6"
+
+libraryDependencies ++= Seq( jdbc , anorm , cache , ws )
+
+unmanagedResourceDirectories in Test <+=  baseDirectory ( _ /"target/web/public/test" )
+
+libraryDependencies ++= Seq(
+  cache,
+  ws,
+  "org.webjars" %% "webjars-play" % "2.4.0-1",
+  "org.webjars.bower" % "angular" % "1.4.7",
+  "org.webjars" % "jquery" % "2.1.1",
+  "org.webjars" % "bootstrap" % "3.2.0-2",
+  "org.webjars.bower" % "angular-route" % "1.4.7"
+)
---
+++ public/javascripts/controllers.js
@@ -0,0 +1,34 @@
+var propertiesApp = angular.module('propertiesApp', []);
+
+//propertiesApp.controller('PropertiesListCtrl', function ($scope) {
+//    $scope.views.properties = [
+//        {name: 'Property1'},
+//        {name: 'Property2'},
+//        {name: 'Property3'}
+//    ];
+
+    propertiesApp.controller('PropertiesListCtrl', ['$scope', '$http',
+        function ($scope, $http) {
+            $http.get("properties/properties.json").success(function(data) {
+                $scope.properties = data;
+            });
+
+
+    $scope.selectedProperties = [ ];
+
+    $scope.countSelected = 0;
+
+
+    $scope.select = function(property) {
+        $scope.selectedProperties.push(property);
+    };
+
+    $scope.processed = function() {
+        if ($scope.selectedProperties != 2) $scope.showError
+    };
+
+    $scope.showError = function() {
+        //TODO всплывающее окно
+    };
+}]);
+
